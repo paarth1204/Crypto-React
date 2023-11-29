948c519e023a06f462cf3bf6c65f3cc03073f270
paarth1204 <paarth1204rastogi@gmail.com>
2023-11-29 16:15:34 +0530

    Correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,11 @@ function Header() {
         Coins React
       </Link>
 
-      <input
+      {/* <input
         type="text"
         placeholder="Enter Coin"
         className="px-4 py-3 rounded-full transition-all duration-300 bg-stone-200 focus:border-sky-100 sm:w-64 sm:focus:w-72"
-      />
+      /> */}
     </header>
   );
 }
